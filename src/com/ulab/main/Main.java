bc2cd9c25902b7063899c2eba9e8bec5dd9723e9
TngDs <tonygms2@gmail.com>
2019-04-27 22:49:08 +0600

    Connected program to jtable

--- src/com/ulab/main/Main.java
+++ src/com/ulab/main/Main.java
@@ -8,17 +8,18 @@ public class Main {
 
 	public static void main(String[] args) throws SQLException {
 		
-		Faculty mizan = new Faculty("Mizanur Rahman",2441139, "CSE",gender.male);
-		mizan.setDOB(23, 11, 1977);
-		mizan.setMemebership(true, true);
-		mizan.showFacultyInfo();//sysouts all info
-		//System.out.println(mizan.showDOB());//returns DOB in strings
-		Student tony = new Student("Nicholas Tony Gomes",172014005, "CSE", gender.male);
-		tony.setDOB(9, 3, 1997);
-		tony.setMemebership(true, false);
-		tony.showStudentInfo();//sysouts all info
+//		Faculty mizan = new Faculty("Mizanur Rahman",2441139, "CSE",gender.male);
+//		mizan.setDOB(23, 11, 1977);
+//		mizan.setMemebership(true, true);
+//		mizan.showFacultyInfo();//sysouts all info
+//		//System.out.println(mizan.showDOB());//returns DOB in strings
+//		Student tony = new Student("Nicholas Tony Gomes",172014005, "CSE", gender.male);
+//		tony.setDOB(9, 3, 1997);
+//		tony.setMemebership(true, false);
+//		tony.showStudentInfo();//sysouts all info
 		connectionMySQL obj = new connectionMySQL();
 		obj.setConnection();
+		//obj.getStatement();
 	//	System.out.println(tony.showDOB());//returns DOB in strings
 
 	}
--- src/com/ulab/main/connectionMySQL.java
+++ src/com/ulab/main/connectionMySQL.java
@@ -6,33 +6,43 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 
 public class connectionMySQL {
 	private Connection connect = null;
 	private Statement statement = null;
 	private PreparedStatement preparedStatement = null;
 	private ResultSet resultset = null;
-	String query = "select * from jtable;";
+	
 	
 	void setConnection() {
 		try {
-			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "1234");
+			Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/finalproj", "root", "1234");
 			System.out.println("Connected");
 			
+			String query = "SELECT * FROM jtable";
+			statement = connect.createStatement();
+			resultset = statement.executeQuery(query);
+			
+			while (resultset.next()) {
+				String id = resultset.getString("ID");
+				String Name = resultset.getString("NAME");
+				String Dept = resultset.getString("DEPT");
+				boolean gender = resultset.getBoolean("GENDER");
+				
+				System.out.format("%s\n%s\n%s\n%s",id,Name,Dept,gender);
+			}
+			statement.close();
+			
+					
 		} catch (Exception e) {
-			System.err.println(e);
+			System.err.println(e.getMessage());
 		}
 	}
 	
 	
-	void getStatement() throws SQLException {
-		
-		statement = connect.createStatement();
-		resultset = statement.executeQuery(query);
-		
-		int ID = resultset.getInt("ID");
-		System.out.println(ID);
-		
-		
-	}
+//	void getStatement() throws SQLException {
+//		
+//		
+//	}
 }
